b8807ca382330694afab92c25574ca6a23fce94f
Anand Patil <anand.hp@gmail.com>
2014-07-02 20:39:18 +0530

    Symmetric tree

---
+++ Tree/SymmetricTree.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "TreeNode.h"
+
+using namespace std;
+
+class Solution {
+
+public:
+    bool isSymmetric(TreeNode* left, TreeNode* right) {
+        if (!left && !right)
+            return true;
+        if (left && right && left->val == right->val) {
+            return isSymmetric(left->left, right->right) && isSymmetric(left->right, right->left);
+        }
+        return false;
+    }
+    
+    bool isSymmetric(TreeNode *root) {
+        if (!root)
+            return true;
+        return isSymmetric(root->left, root->right);
+    }
+};
+
+
+int main()
+{
+    string unSymmetric = "30 10 50 # # # 20 45 # # 35"; //unsymmetric tree
+    string symmetric = "1 2 3 # # 4 # # 2 4 # # 3"; //symmetric tree
+    
+    TreeNode* uroot = buildTree(unSymmetric);
+    TreeNode* sroot = buildTree(symmetric);
+    
+    Solution sl;
+    
+    cout<<"->"<<sl.isSymmetric(uroot)<<endl;
+    cout<<"->"<<sl.isSymmetric(sroot)<<endl;
+    
+    deleteTree(uroot);
+    deleteTree(sroot);
+    
+    return 0;
+}
---
+++ Tree/TreeNode.h
@@ -0,0 +1,103 @@
+/**
+ * Definition for binary tree
+ *
+ **/
+#include <sstream>
+#include <string>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+bool getNextToken(std::stringstream &ss, int &token, bool &isNumber)
+{
+    if (ss.eof()) return false;
+    
+    std::string str = "";
+    char c;
+
+    while((c = ss.get()) == ' ');
+    
+    while (c >= '0' && c <= '9') {
+        str += c;
+        c = ss.get();
+    }
+
+    if (str != "") {
+        token = atoi(str.c_str());
+        isNumber = true;
+    }
+    
+    return true;    
+}
+
+TreeNode* deSerializeTree(std::stringstream &ss) 
+{
+    int value;
+    bool isNumber = false;
+    
+    if (!getNextToken(ss, value, isNumber) || !isNumber)
+        return NULL;
+    
+    TreeNode* root = new TreeNode(value);
+    root->left = deSerializeTree(ss);
+    root->right = deSerializeTree(ss);
+    return root;
+}
+
+TreeNode* buildTree(std::string str)
+{
+    std::stringstream ss;
+    ss<<str;
+    
+    return deSerializeTree(ss);
+}
+
+void deleteTree(TreeNode* root)
+{
+    if (!root)
+        return;
+    deleteTree(root->left);
+    deleteTree(root->right);
+    delete root;    
+}
+
+void treePreorder(TreeNode* root)
+{
+    if (!root) {
+        std::cout<<"# ";
+        return;
+    }
+    std::cout<<root->val<<" ";
+    treePreorder(root->left);
+    treePreorder(root->right);
+}
+
+void treeInorder(TreeNode* root)
+{
+    if (!root) {
+        std::cout<<"# ";
+        return;
+    }
+    
+    treeInorder(root->left);
+    std::cout<<root->val<<" ";
+    treeInorder(root->right);
+    
+}
+
+void treePostorder(TreeNode* root)
+{
+    if (!root) {
+        std::cout<<"# ";
+        return;
+    }
+    
+    treePostorder(root->left);
+    treePostorder(root->right);
+    std::cout<<root->val<<" ";
+}
