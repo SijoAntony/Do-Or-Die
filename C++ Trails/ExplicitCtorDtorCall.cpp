b0b09b1862d395eaf4833646ce4097d92ff40c25
Anand Patil <anand.hp@gmail.com>
2014-07-04 16:23:37 +0530

    Explicit ctor and dtor calls

---
+++ C++ Trails/ExplicitCtorDtorCall.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ 
+class Test
+{
+public:
+    Test()  { cout << "Constructor is executed\n"; }
+    Test(const Test& t)  { cout << "Copy Constructor is executed\n"; }    
+    ~Test() { cout << "Destructor is executed\n";  }
+    friend void fun(Test t);
+};
+void fun(Test t)
+{
+    Test();
+    t.~Test();
+}
+int main()
+{
+    Test();     //Calls temporary Ctor-Dtor
+    Test t;     //Calls t Ctor
+    fun(t);     //Calls t.CopyCtor, temporary Ctor-Dtor, t.Dtor, t.Dtor
+    return 0;   //t.Dtor
+}
