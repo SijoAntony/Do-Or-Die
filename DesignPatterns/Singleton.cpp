ebffc02cc8ca14694f06492913cfeb54fc44f627
Anand Patil <anand.hp@gmail.com>
2014-07-24 09:10:35 +0530

    Design patters

---
+++ DesignPatterns/Singleton.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+class Printer {
+public:
+    static Printer* getInstance() {
+        if (!m_instance) {
+            //Handle thread safetly
+            m_instance = new Printer();
+        }
+        
+        return m_instance;
+    }
+    
+    static void destroy() {
+        delete m_instance;    
+    }
+    
+    void printToConsole(string data) {
+        cout<<data<<endl;
+    }
+    
+    void printToFile(string data) {
+    }
+
+private:
+    Printer() {
+    }
+
+    Printer(const Printer& other) {
+        //avoid self assignment
+        if (this != &other) {
+        }    
+    }
+    
+    static Printer* m_instance;
+};
+
+Printer* Printer::m_instance = NULL;
+
+
+int main()
+{
+    Printer* printer = Printer::getInstance(); // Since new Printer(); fails
+    printer->printToConsole("Hello there!");
+    return 0;    
+}
