94b708f51bfe5450bcd714664e95d6ac3fe036ec
Anand Patil <anand.hp@gmail.com>
2014-08-27 10:50:42 +0530

    Graphs

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -6,69 +6,176 @@
 
 using namespace std;
 
-class Vertex {
-public:
-    Vertex(int id)
-        : m_id(id) {
-    }
-    
-    void addNeighbour(Vertex *v, int weight) {
-        m_neighbours[v] = weight;
-    }
-    
-    void print() {
-        cout<<m_id<<" ";
-        for (map<Vertex*, int>::iterator it = m_neighbours.begin(); it != m_neighbours.end(); ++it)
-            cout<<"("<<it->first->m_id<<":"<<it->second<<")";
-        cout<<endl;
-    }
-    int key() {return m_id;}
-    
-    vector<Vertex*> getNeighbours() {
-        vector<Vertex*> n;
-        for (map<Vertex*, int>::iterator it = m_neighbours.begin(); it != m_neighbours.end(); ++it)
-            n.push_back(it->first);
-        
-        return n;
-    }
-    
-private:
-    int m_id;
-    map<Vertex*, int> m_neighbours;
+struct UndirectedGraphNode {
+    int label;
+    vector<UndirectedGraphNode *> neighbors;
+    UndirectedGraphNode(int x) : label(x) {};
 };
 
-class Graph {
+class UndirectedGraph {
 public:
-    Graph()
+    UndirectedGraph()
         : m_size(0) {
     }
     
     void addVertex(int key) {
         m_size++;
-        m_vertices[key] = new Vertex(key);
+        m_vertices[key] = new UndirectedGraphNode(key);
     }
     
-    void addEdge(int from, int to, int weight) {
-        m_vertices[from]->addNeighbour(m_vertices[to], weight);
+    void addEdge(int from, int to) {
+        m_vertices[from]->neighbors.push_back(m_vertices[to]);
     }
     
-    vector<int> getVertices() {
-        vector<int> vertices(m_size);
-        for (map<int, Vertex*>::iterator it = m_vertices.begin(); it != m_vertices.end(); ++it)
-            vertices.push_back(it->first);
+    vector<UndirectedGraphNode*> getVertices() {
+        vector<UndirectedGraphNode*> vertices(m_size);
+        for (map<int, UndirectedGraphNode*>::iterator it = m_vertices.begin(); it != m_vertices.end(); ++it)
+            vertices.push_back(it->second);
         return vertices;
     }
     
-    Vertex* getVertex(int key) {
+    UndirectedGraphNode* getVertex(int key) {
         return m_vertices[key];
     }
     
+    static void bfs(UndirectedGraphNode* n) {
+        if (!n)
+            return;
+            
+        queue<UndirectedGraphNode*> q;
+        map<UndirectedGraphNode*, bool> visited;
+        q.push(n);
+        
+        while (!q.empty()) {
+            UndirectedGraphNode* node = q.front();
+            q.pop();
+            
+            visited[node] = true;
+            cout<<node->label<<" ( ";
+            for (vector<UndirectedGraphNode*>::iterator it = node->neighbors.begin(); it != node->neighbors.end(); ++it) {
+                cout<<(*it)->label<<" ";
+                if (visited.find(*it) == visited.end())
+                    q.push(*it);
+                visited[*it] = true;
+            }
+            cout<<")"<<endl;
+        }
+    }
+    
     void print(){
-        for (map<int, Vertex*>::iterator it = m_vertices.begin(); it != m_vertices.end(); ++it)
-            it->second->print();
+        for (map<int, UndirectedGraphNode*>::iterator it = m_vertices.begin(); it != m_vertices.end(); ++it) {
+            UndirectedGraphNode *v = it->second; 
+            cout<<v->label<<" ( ";
+            for (vector<UndirectedGraphNode*>::iterator n = v->neighbors.begin(); n != v->neighbors.end(); ++n)
+                cout<<(*n)->label<<" ";
+            cout<<")"<<endl;
+        
+        }
     }
     
 private:
-    map<int, Vertex*> m_vertices;
+    map<int, UndirectedGraphNode*> m_vertices;
+    int m_size;
+};
+
+struct Edge;
+
+struct DirectedGraphNode {
+    int label;
+    vector<Edge*> neighbors;
+    DirectedGraphNode(int x) : label(x) {}
+};
+
+struct Edge {
+    DirectedGraphNode* node;
+    int weight;
+    Edge(DirectedGraphNode* _node, int _weight) 
+        : node(_node)
+        , weight(_weight) {}
+};
+
+class DirectedGraph {
+public:
+    DirectedGraph()
+        : m_size(0) {
+    }
+    
+    void addVertex(int key) {
+        m_size++;
+        m_vertices[key] = new DirectedGraphNode(key);
+    }
+    
+    void addEdge(int from, int to, int weight) {
+        m_vertices[from]->neighbors.push_back(new Edge(m_vertices[to], weight));
+    }
+    
+    vector<DirectedGraphNode*> getVertices() {
+        vector<DirectedGraphNode*> vertices(m_size);
+        for (map<int, DirectedGraphNode*>::iterator it = m_vertices.begin(); it != m_vertices.end(); ++it)
+            vertices.push_back(it->second);
+        return vertices;
+    }
+    
+    DirectedGraphNode* getVertex(int key) {
+        return m_vertices[key];
+    }
+    
+    static void bfs(DirectedGraphNode* n) {
+        if (!n)
+            return;
+            
+        queue<DirectedGraphNode*> q;
+        map<DirectedGraphNode*, bool> visited;
+        q.push(n);
+        
+        while (!q.empty()) {
+            DirectedGraphNode* node = q.front();
+            q.pop();
+            
+            visited[node] = true;
+            cout<<node->label<<" (";
+            for (vector<Edge*>::iterator it = node->neighbors.begin(); it != node->neighbors.end(); ++it) {
+                DirectedGraphNode *neighbor = (*it)->node;
+                cout<<"["<<neighbor->label<<" "<<(*it)->weight<<"]";
+                if (visited.find(neighbor) == visited.end())
+                    q.push(neighbor);
+                visited[neighbor] = true;
+            }
+            cout<<")"<<endl;
+        }
+    }
+
+    static void dfs(DirectedGraphNode *n, map<DirectedGraphNode *, bool> &visited) {
+        if (!n)
+            return;
+
+        visited[n] = true;
+
+        for (int i = 0; i < n->neighbors.size(); i++) {
+            Edge *edge = n->neighbors[i];
+            if (!visited[edge->node]) {
+                cout<<n->label<<" ";
+                cout<<"["<<edge->node->label<<" "<<edge->weight<<"] ";
+                dfs(edge->node, visited);
+            }
+        }
+    }
+    
+    static void dfs(DirectedGraphNode *n) {
+        map<DirectedGraphNode *, bool> visited;
+        dfs(n, visited);        
+    }    
+    void print(){
+        for (map<int, DirectedGraphNode*>::iterator it = m_vertices.begin(); it != m_vertices.end(); ++it) {
+            DirectedGraphNode *v = it->second; 
+            cout<<v->label<<" (";
+            for (vector<Edge*>::iterator n = v->neighbors.begin(); n != v->neighbors.end(); ++n)
+                cout<<"["<<(*n)->node->label<<" "<<(*n)->weight<<"]";
+            cout<<")"<<endl;
+        }
+    }
+    
+private:
+    map<int, DirectedGraphNode*> m_vertices;
     int m_size;
 };
--- Graph/SampleGraph.cpp
+++ Graph/SampleGraph.cpp
@@ -1,45 +1,13 @@
 #include <iostream>
 #include <queue>
-#include <stack>
 
 #include "Graph.h"
 
 using namespace std;
 
-void bfs(Graph& g, int s)
-{
-    map<Vertex*, bool> visitedVertices;
-    vector<int> vertices = g.getVertices();
-    
-    for (vector<int>::iterator it = vertices.begin(); it != vertices.end(); ++it)
-        visitedVertices[g.getVertex(*it)] = false;
-        
-    queue<Vertex*> Q;
-    Q.push(g.getVertex(vertices[0]));
-    
-    while (!Q.empty()) {
-        Vertex  *v = Q.front();
-        Q.pop();
-
-        if(visitedVertices[v])
-            continue;
-            
-        cout<<v->key()<<" ";
-        visitedVertices[v] = true;
-        vector<Vertex*> n = v->getNeighbours();
-        
-        for(int i = 0; i < n.size(); i++)
-                Q.push(n[i]);        
-    }
-}
-
-void dfs(Graph& g, int s)
-{
-
-}
 int main()
 {
-    Graph g;
+    DirectedGraph g;
     for (int i = 0; i < 6; i++)
         g.addVertex(i);
     
@@ -54,8 +22,7 @@ int main()
     g.addEdge(5,2,1);         
     
     g.print();
-    
-    bfs(g, 0);
-                
+
+    DirectedGraph::dfs(g.getVertex(0));
     return 0;
 }
