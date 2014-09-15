affea7324eef84b31210ee16e10ec7871812a44c
Anand Patil <anand.hp@gmail.com>
2014-09-16 00:03:53 +0530

    Test pad template

--- Arrays/TestPad.cpp
+++ Arrays/TestPad.cpp
@@ -1,24 +1,13 @@
 #include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+
+#include "ArrayUtils.h"
 
 using namespace std;
 
-int numTrees(int n) {
-    if (n <= 1)
-        return 1;
-
-    int trees = 0, left = 0, right = 0;
-
-    for (int i = 1; i <= n; i++) {
-        left = numTrees(i - 1);
-        right = numTrees(n - i);
-        trees += left * right;
-    }
-
-    return trees;
-}
-
 int main()
 {
-    cout<<"Num of trees "<<numTrees(4)<<endl;
     return 0;
 }
